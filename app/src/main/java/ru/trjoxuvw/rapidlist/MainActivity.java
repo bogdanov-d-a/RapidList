af4238479da4108711eb70f315fd2b47e76a33da
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2019-07-21 22:23:09 +0300

    Update project

--- app/src/main/java/ru/trjoxuvw/rapidlist/MainActivity.java
+++ app/src/main/java/ru/trjoxuvw/rapidlist/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.trjoxuvw.rapidlist;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
